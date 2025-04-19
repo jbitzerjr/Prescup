ef5df94e8ce9cbdca315e62e11f1880da889ce48
jbitzer <jbitzer@fastmail.fm>
2025-04-18 22:28:02 -0400

    made changes to autodeploy with newly generated flags.

--- utils/init.sh
+++ utils/init.sh
@@ -1,26 +1,43 @@
-shop-smart:~/shop-smart# cd ..
-shop-smart:~# cat init.sh 
 #!/bin/sh
 
 ADJECTIVES="fuzzy tartan haunted evil cryptic dumb cursed filthy moldy broken"
 NOUNS="squirrel chainsaw gremlin possum priest curse toaster goblin"
+
 FLAG_LOG="/root/current_tokens.txt"
 > "$FLAG_LOG"
 
-# Find total number of placeholders
-TOTAL=$(grep -r "TOKEN: ########" /root | wc -l)
-echo "Found $TOTAL flag slots."
+# Detect the challenge directory
+CHALLENGE_DIR=$(find /root -mindepth 1 -maxdepth 1 -type d ! -name '.*' ! -name 'lost+found')
+cd "$CHALLENGE_DIR" || exit 1
 
-# Replace each one with a unique flag
-for i in $(seq 1 $TOTAL); do
-  ADJ=$(shuf -n1 -e $ADJECTIVES)
-  NOUN=$(shuf -n1 -e $NOUNS)
-  HEX=$(head -c4 /dev/urandom | base64 | tr -dc 'a-f0-9' | head -c4)
-  FLAG="{flag} ${ADJ}-${NOUN}-${HEX}"
+# Find all .php and .txt files with ########
+PLACEHOLDER_FILES=$(find "$CHALLENGE_DIR" -type f \( -name '*.php' -o -name '*.txt' \) -exec grep -l "########" {} +)
 
-  find /root -type f -name '*.php' -exec sed -i "0,/TOKEN: ########/s//${FLAG}/" {} +
-  echo "FLAG $i: $FLAG" >> "$FLAG_LOG"
+i=1
+for FILE in $PLACEHOLDER_FILES; do
+  COUNT=$(grep -o "########" "$FILE" | wc -l)
+  for _ in $(seq 1 $COUNT); do
+    ADJ=$(shuf -n1 -e $ADJECTIVES)
+    NOUN=$(shuf -n1 -e $NOUNS)
+    HEX=$(hexdump -n 2 -e '4/1 "%02x"' /dev/urandom)
+    FLAG="{flag} ${ADJ}-${NOUN}-${HEX}"
+
+    awk -v flag="$FLAG" '
+      BEGIN { replaced=0 }
+      {
+        if (!replaced && /########/) {
+          sub(/########/, flag)
+          replaced=1
+        }
+        print
+      }
+    ' "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
+
+    SHORTFILE=$(basename "$FILE")
+    echo "FLAG $i ($SHORTFILE): $FLAG" >> "$FLAG_LOG"
+    i=$((i+1))
+  done
 done
 
-cd /root/shop-smart # change to the shop-smart directory, you need to be in the same directory as the docker-compose.yml file, so alter this line if needed.
+# Start the challenge
 docker compose up -d
